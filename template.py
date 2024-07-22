67a2be27dcd6e7e7e5cc40a87ed14edbdae5ee81
mrfaridnaz <mrfad8392@gmail.com>
2024-07-22 22:10:22 +0530

    Requirments added

--- template.py
+++ template.py
@@ -6,8 +6,6 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "mlProject"
 
-
-
 list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -28,11 +26,7 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
-
-
 ]
-
-
 
 for filepath in list_of_files:
     filepath = Path(filepath)
